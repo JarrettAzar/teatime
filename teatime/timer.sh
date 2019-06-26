834a5ab0a74633422b254a7940c0584e4b9dccb9
Jarrett Azar <30982357+JarrettAzar@users.noreply.github.com>
2019-06-26 18:57:39 -0400

    Add files via upload

--- teatime/timer.sh
+++ teatime/timer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#This was modified by Diamond :) (he made it more concise)
+
 teatype=$(< /tmp/teatype)
 
 seconds=$[ teatype * 60 ]
@@ -11,3 +13,9 @@ while [[ "$date1" -ge `now` ]]; do
   date -u --date @$[ date1 - `now` ] +%M:%S
   sleep 1
 done
+
+case "$1" in
+    1) notify-send "Tea Finished!" "Green Tea" ;;
+    3) notify-send "Tea Finished!" "Black Tea" ;;
+    5) notify-send "Tea Finished!" "Herbal Tea"
+esac
